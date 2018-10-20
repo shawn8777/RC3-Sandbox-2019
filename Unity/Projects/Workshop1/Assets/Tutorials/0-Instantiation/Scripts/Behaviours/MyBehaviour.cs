c39554b9593a07bcfe64037bee028c8a5e837521
David Reeves <darthur.reeves@gmail.com>
2018-10-20 13:40:55 +0100

    First commit

---
+++ Unity/Projects/Workshop1/Assets/Tutorials/0-Instantiation/Scripts/Behaviours/MyBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using SpatialSlur;
+
+namespace RC3
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MyBehaviour : MonoBehaviour
+    {
+        private void Awake()
+        {
+        }
+
+        void Start()
+        {
+        }
+        
+        void Update()
+        {
+        }
+    }
+}
---
+++ Unity/Projects/Workshop1/Assets/Tutorials/0-Instantiation/Scripts/Objects/MyObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using SpatialSlur;
+
+namespace RC3
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MyObject : ScriptableObject
+    {
+    }
+}
