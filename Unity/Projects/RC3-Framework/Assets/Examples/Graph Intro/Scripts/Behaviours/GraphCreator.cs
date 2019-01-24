ee90ee957152e87f67ac2cb460d58e538beb64f8
Dave Reeves <darthur.reeves@gmail.com>
2019-01-24 22:05:16 +0000

    Add tutorial content

---
+++ Unity/Projects/RC3-Framework/Assets/Examples/Graph Intro/Scripts/Behaviours/GraphCreator.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RC3.Unity.GraphIntro
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class GraphCreator : MonoBehaviour
+    {
+        [SerializeField] private Graph _graph;
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void Start()
+        {
+            CreateGraph();
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void CreateGraph()
+        {
+            var verts = _graph.Vertices;
+            const int nv = 6;
+
+            // Add vertices
+            {
+                for (int i = 0; i < nv; i++)
+                    verts.Add(new List<int>());
+
+                Debug.Log($"The graph has {verts.Count} vertices.");
+            }
+
+            // Add edges
+            {
+                verts[0].Add(2);
+                verts[2].Add(0);
+
+                verts[1].Add(3);
+                verts[3].Add(1);
+
+                verts[2].Add(3);
+                verts[3].Add(2);
+
+                verts[2].Add(4);
+                verts[4].Add(2);
+
+                verts[3].Add(5);
+                verts[5].Add(3);
+
+                for (int i = 0; i < nv; i++)
+                    Debug.Log($"Vertex {i} is degree {verts[i].Count}.");
+            }
+
+            // Add positions
+            {
+                var positions = _graph.Positions;
+
+                positions.Add(new Vector3(0.0f, 0.0f, 2.0f));
+                positions.Add(new Vector3(4.0f, 0.0f, 2.0f));
+
+                positions.Add(new Vector3(1.0f, 0.0f, 1.0f));
+                positions.Add(new Vector3(3.0f, 0.0f, 1.0f));
+
+                positions.Add(new Vector3(0.0f, 0.0f, 0.0f));
+                positions.Add(new Vector3(4.0f, 0.0f, 0.0f));
+            }
+        }
+    }
+}
---
+++ Unity/Projects/RC3-Framework/Assets/Examples/Graph Intro/Scripts/Behaviours/GraphDebugDisplay.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+
+namespace RC3.Unity.GraphIntro
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class GraphDebugDisplay : DebugDisplay
+    {
+        [SerializeField] private Graph _graph;
+
+        [SerializeField] Color _color0 = Color.white;
+        [SerializeField] Color _color1 = Color.black;
+
+        [SerializeField] float _scale0 = 1.0f;
+        [SerializeField] float _scale1 = 0.5f;
+
+        [SerializeField] private float _period = 3.0f; // Time in seconds per cycle
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected override void Display()
+        {
+            DisplayFancy();
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void DisplayFancy()
+        {
+            var verts = _graph.Vertices;
+            var positions = _graph.Positions;
+
+            // 0 <= t < = 1
+            float t = Mathf.Sin(Time.time / _period * Mathf.PI * 2.0f) * 0.5f + 0.5f;
+
+            Color color = Color.Lerp(_color0, _color1, t);
+            float scale = Mathf.Lerp(_scale0, _scale1, t);
+
+            GL.Begin(GL.LINES);
+
+            GL.Color(color);
+
+            for (int i = 0; i < verts.Count; i++)
+            {
+                foreach (var j in verts[i])
+                {
+                    if (j > i)
+                    {
+                        var p0 = positions[i];
+                        var p1 = positions[j];
+                        var mid = (p0 + p1) * 0.5f;
+
+                        GL.Vertex(Vector3.Lerp(mid, p0, scale));
+                        GL.Vertex(Vector3.Lerp(mid, p1, scale));
+                    }
+                }
+            }
+
+            GL.End();
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void DisplaySimple()
+        {
+            var verts = _graph.Vertices;
+            var positions = _graph.Positions;
+
+            GL.Begin(GL.LINES);
+
+            GL.Color(Color.white);
+
+            for (int i = 0; i < verts.Count; i++)
+            {
+                foreach (var j in verts[i])
+                {
+                    if (j > i)
+                    {
+                        GL.Vertex(positions[i]);
+                        GL.Vertex(positions[j]);
+                    }
+                }
+            }
+
+            GL.End();
+        }
+    }
+}
