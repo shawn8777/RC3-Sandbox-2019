a185a1de7c95ee3c50fbf6af9cd459411db1319a
Dave Reeves <darthur.reeves@gmail.com>
2019-01-29 23:52:14 +0000

    Add tutorial content

---
+++ Unity/Projects/RC3-Framework/Assets/Examples/Graph Intro/Scripts/Behaviours/GraphDegreeDisplay.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace RC3.Unity.GraphIntro
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class GraphDegreeDisplay : DebugDisplay
+    {
+        [SerializeField] private Graph _graph;
+        [SerializeField] private Texture2D _gradient;
+
+        [SerializeField] private int _minDegree = 0;
+        [SerializeField] private int _maxDegree = 10;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected override void Display()
+        {
+            var verts = _graph.Vertices;
+            var positions = _graph.Positions;
+
+            GL.Begin(GL.LINES);
+            
+            for (int i = 0; i < verts.Count; i++)
+            {
+                float t0 = Mathf.InverseLerp(_minDegree, _maxDegree, verts[i].Count);
+                Color c0 = _gradient.GetPixelBilinear(t0, t0);
+
+                foreach (var j in verts[i])
+                {
+                    if (j > i)
+                    {
+                        float t1 = Mathf.InverseLerp(_minDegree, _maxDegree, verts[j].Count);
+                        Color c1 = _gradient.GetPixelBilinear(t1, t1);
+
+                        GL.Color(c0);
+                        GL.Vertex(positions[i]);
+
+                        GL.Color(c1);
+                        GL.Vertex(positions[j]);
+                    }
+                }
+            }
+
+            GL.End();
+        }
+    }
+}
---
+++ Unity/Projects/RC3-Framework/Assets/Examples/Graph Intro/Scripts/GraphFactory.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace RC3.Unity.GraphIntro
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class GraphFactory
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void CreateTestGraph0(Graph graph)
+        {
+            var verts = graph.Vertices;
+            const int nv = 6;
+
+            // Add vertices
+            {
+                for (int i = 0; i < nv; i++)
+                    verts.Add(new List<int>());
+
+                Debug.Log($"The graph has {verts.Count} vertices.");
+            }
+
+            // Add edges
+            {
+                verts[0].Add(2);
+                verts[2].Add(0);
+
+                verts[1].Add(3);
+                verts[3].Add(1);
+
+                verts[2].Add(3);
+                verts[3].Add(2);
+
+                verts[2].Add(4);
+                verts[4].Add(2);
+
+                verts[3].Add(5);
+                verts[5].Add(3);
+
+                for (int i = 0; i < nv; i++)
+                    Debug.Log($"Vertex {i} is degree {verts[i].Count}.");
+            }
+
+            // Add positions
+            {
+                var positions = graph.Positions;
+
+                positions.Add(new Vector3(0.0f, 0.0f, 2.0f));
+                positions.Add(new Vector3(4.0f, 0.0f, 2.0f));
+
+                positions.Add(new Vector3(0.0f, 0.0f, 1.0f));
+                positions.Add(new Vector3(4.0f, 0.0f, 1.0f));
+
+                positions.Add(new Vector3(0.0f, 0.0f, 0.0f));
+                positions.Add(new Vector3(4.0f, 0.0f, 0.0f));
+            }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="graph"></param>
+        public static void CreateScatterGraph(Graph graph, float maxRadius, int vertexCount)
+        {
+            var verts = graph.Vertices;
+            var positions = graph.Positions;
+
+            var random = new System.Random(1);
+
+            // Add vertices and positions
+            for (int i = 0; i < vertexCount; i++)
+            {
+                float x = (float)random.NextDouble();
+                float z = (float)random.NextDouble();
+
+                positions.Add(new Vector3(x, 0.0f, z));
+                verts.Add(new List<int>());
+            }
+
+            // Add edges
+            AddEdgesByRange(graph, maxRadius);
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="graph"></param>
+        /// <param name="maxRadius"></param>
+        public static void AddEdgesByRange(Graph graph, float maxRadius)
+        {
+            var verts = graph.Vertices;
+            var positions = graph.Positions;
+            float sqrRad = maxRadius * maxRadius;
+            
+            for (int i = 0; i < verts.Count; i++)
+            {
+                for (int j = i + 1; j < verts.Count; j++)
+                {
+                    var sqrDist = (positions[i] - positions[j]).sqrMagnitude;
+
+                    if (sqrDist < sqrRad)
+                    {
+                        verts[i].Add(j);
+                        verts[j].Add(i);
+                    }
+                }
+            }
+        }
+    }
+}
